01043c74ce789b8c1181220abd216a6dfe74cb20
yayo1983 <yazanenator@gmail.com>
2024-06-09 12:16:16 -0600

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,12 @@
+# myapp/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookListCreateView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
